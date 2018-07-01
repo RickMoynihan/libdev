53f52d60fd9ecb0ffe5f85a4bec6fa19b30cea2d
Rick Moynihan <rick.m@swirrl.com>
2018-07-01 19:42:56 +0100

    Version 0.2.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject libdev/clj-template "0.2.0-SNAPSHOT"
+(defproject libdev/clj-template "0.2.0"
   :description "A clj-new template for building Clojure libraries with bespoke a dev environment & tooling"
   :url "https://github.com/RickMoynihan/libdev"
   :license {:name "Eclipse Public License"
